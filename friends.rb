bf35162e33efafca6ff00a4a3e3f0bb9caba7dd6
Matisse Mallette <matissemallette@gmail.com>
2022-08-03 10:44:41 -0700

    Add a new array variable containing locations

--- friends.rb
+++ friends.rb
@@ -3,3 +3,7 @@ array_var = ["Ben", "Dave", "Seth", "Courtney", "Michael"]
 puts(array_var[2])
 puts(array_var[-1])
 puts(array_var[0])
+
+array_var2 = ["France", "Germany", "China", "Mexico", "Sweden"]
+
+array_var2.push("England")
